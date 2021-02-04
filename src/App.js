1dd7c1e3aaefe7ce00e073b4e1403b70ac4d78a0
Alexander Alemayhu <alexander@alemayhu.com>
2021-02-04 13:35:42 +0100

    get the page rendering as expected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+const links = [
+  { title: "YouTube", link: "https://youtube.com/c/alexanderalemayhu" },
+  { title: "Telegram", link: "https://t.me/joinchat/TkdYqYimUj-lp7JD" },
+  { title: "Tiktok", link: "https://www.tiktok.com/@alexanderalemayhu" },
+  { title: "Discord", link: "https://discord.gg/PSKC3uS" },
+  { title: "GitHub Sponsor", link: "http://github.com/sponsors/alemayhu" },
+  { title: "Patreon", link: "https://www.patreon.com/alemayhu" },
+  { title: "Website", link: "https://alemayhu.com" },
+  { title: "Twitter", link: "https://twitter.com/AAlemayhu" },
+  { title: "Buy me a coffee", link: "https://buymeacoffee.com/aalemayhu" },
+  { title: "Tip", link: "https://streamlabs.com/alexanderalemayhu1" },
+  { title: "Ko-Fi", link: "https://ko-fi.com/alemayhu" },
+  { title: "Paypal", link: "https://paypal.me/alemayhu" },
+  { title: "Twitch", link: "http://twitch.tv/alexanderalemayhu" },
+  { title: "AlemayhuVODS", link: "https://www.youtube.com/c/AlemayhuVODS" }
+];
 
 function App() {
   return (
@@ -7,17 +24,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Alexander Alemayhu
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        {links.map((l) => <a  className="link-a" target="_blank" rel="noreferrer" href={l.link}><div className="link">{l.title}</div></a>)}
+      </div>
+      <footer>
+        <p class="footer-text">
+          The source code is available on  <a href="https://github.com/alemayhu/links">GitHub</a>                              
+        </p>
+      </footer>
     </div>
   );
 }
