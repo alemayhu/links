592207418127f5e0cd6cb4ff37d97b230b332b14
Alexander Alemayhu <alexander@alemayhu.com>
2021-04-05 08:57:57 +0200

    make the links visible by default
    Also finally resolve the background not being fixed to the whole page
    when toggling the toggles.

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,7 @@ import Link from './Link';
 const Content = ({ links }) => {
         return (
                 <div>
-                        {Object.keys(links).map((name) => <details key={name}>
+                        {Object.keys(links).map((name) => <details key={name} open>
                                 <summary>{name}</summary>
                                 {links[name].map(l => <Link {...l} key={l.title} />)}
                         </details>
