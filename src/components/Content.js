002b90577a8eae6bfe33bfab44f106e6eb600268
Alexander Alemayhu <alexander@alemayhu.com>
2021-07-10 21:12:33 +0200

    links: make the links look more compact

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,7 @@ import Link from './Link';
 const Content = ({ links }) => {
         return (
                 <div>
-                        {Object.keys(links).map((name) => <details key={name} open>
+                        {Object.keys(links).map((name) => <details key={name}>
                                 <summary>{name}</summary>
                                 {links[name].map(l => <Link {...l} key={l.title} />)}
                         </details>
