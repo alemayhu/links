f645f4d5f59b1f283f54892a7928d4be15ca8417
Alexander Alemayhu <alexander@alemayhu.com>
2021-07-24 12:24:05 +0200

    add paragraph before links
    Hoping to reduce the bounce rate.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,22 @@
+import styled from 'styled-components';
 import Link from './Link';
+
+const StyledParagraph = styled.p`
+        max-width: 500px
+`;
 
 const Content = ({ links }) => {
         return (
-                <div>
-                        {Object.keys(links).map((name, index) => <details key={name} open={index === 0}>
-                                <summary>{name}</summary>
-                                {links[name].map(l => <Link {...l} key={l.title} />)}
-                        </details>
-                        )}
-                </div>
+                <>
+                        <StyledParagraph>Hei there!  Welcome to my links page. I hope you find what you are looking for below. If not feel free to contact me.</StyledParagraph>
+                        <div>
+                                {Object.keys(links).map((name, index) => <details key={name} open={index === 0}>
+                                        <summary>{name}</summary>
+                                        {links[name].map(l => <Link {...l} key={l.title} />)}
+                                </details>
+                                )}
+                        </div>
+                </>
 
         );
 };
