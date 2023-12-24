b13063ad79790c2ca040d683178125ee08bf5268
Alexander Alemayhu <alexander@alemayhu.com>
2023-12-25 00:00:41 +0100

    feat: clean up the links page

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,12 @@
-import styled from 'styled-components';
 import Link from './Link';
-
-const StyledParagraph = styled.p`
-  max-width: 500px;
-`;
 
 const Content = ({ links }) => {
   return (
-    <>
-      <StyledParagraph>
-        Hei there! Welcome to my links page. I hope you find what you are
-        looking for below. If not feel free to contact me.
-      </StyledParagraph>
-      <div>
-        {links.map((link) => (
-          <Link {...link} key={link.title} />
-        ))}
-      </div>
-    </>
+    <div>
+      {links.map((link) => (
+        <Link {...link} key={link.title} />
+      ))}
+    </div>
   );
 };
 
