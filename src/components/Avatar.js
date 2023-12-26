01a8958bde6416c64cfae24856e320c108950415
Alexander Alemayhu <alexander@alemayhu.com>
2023-12-26 14:29:54 +0100

    feat: update the border on the profile image

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -6,7 +6,7 @@ import logo from '../logo.png';
 const StyledImage = styled.img`
   width: 96px;
   height: 96px;
-  border-radius: 50%;
+  border-radius: 50% 10%;
   background: blanchedalmond;
   pointer-events: none;
   object-fit: contain;
